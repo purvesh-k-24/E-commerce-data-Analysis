477118d73356b6ffd09ad98221941769b9f67a96
purvesh-k-24 <purveshkanfade0@gmail.com>
2026-09-11 13:43:19 +0530

    updated customer shopping analysis

--- ecommerce_analysis.py
+++ ecommerce_analysis.py
@@ -38,4 +38,52 @@ print(sales_by_category.head())
 fig = px.pie(sales_by_category, names='Category', values='Sales', hole=0.5, color_discrete_sequence=px.colors.qualitative.Plotly)
 fig.update_traces(textposition='inside', textinfo='percent+label')
 fig.update_layout(title_text='Sales by Category', title_font=dict(size=20))
-fig.show() 
+#fig.show() 
+
+# Sales by Sub-Category
+sales_by_subcategory = data.groupby(['Sub-Category'])['Sales'].sum().reset_index()
+print(sales_by_subcategory.head())
+fig = px.bar(sales_by_subcategory, x='Sub-Category', y='Sales', title='Sales by Sub-Category')
+#fig.show()
+
+# Monthly profit analysis
+profit_by_month = data.groupby(['Order Month'])['Profit'].sum().reset_index()
+print(profit_by_month.head())
+fig = px.line(profit_by_month, x='Order Month', y='Profit', title='Monthly Profit Analysis')
+#fig.show() 
+
+# profit by category
+profit_by_category = data.groupby(['Category'])['Profit'].sum().reset_index()
+print(profit_by_category.head())
+fig = px.pie(profit_by_category, names='Category', values='Profit', hole=0.5, color_discrete_sequence=px.colors.qualitative.Pastel)
+fig.update_traces(textposition='inside', textinfo='percent+label')
+#fig.show()
+
+# profit by sub-category
+profit_by_subcategory = data.groupby(['Sub-Category'])['Profit'].sum().reset_index()
+fig = px.bar(profit_by_subcategory, x='Sub-Category', y='Profit', title='Profit by Sub-Category')
+#fig.show()
+
+# sales and profit customer segment
+sales_profit_by_segment = data.groupby(['Segment'])[['Sales', 'Profit']].sum().reset_index()
+color_palette = colors.qualitative.Pastel
+
+fig = go.Figure()
+fig.add_trace(go.Bar(x=sales_profit_by_segment['Segment'], 
+                     y=sales_profit_by_segment['Sales'],
+                        name='Sales',
+                        marker_color=color_palette[0]))
+
+fig.add_trace(go.Bar(x=sales_profit_by_segment['Segment'],
+                     y=sales_profit_by_segment['Profit'],   
+                       name='Profit',
+                         marker_color=color_palette[1]))
+
+fig.update_layout(title='Sales and Profit by Customer Segment',
+                  xaxis_title='Customer Segment', yaxis_title='Amount')
+#fig.show()
+
+# sales to profit ratio
+sales_profit_segment = data.groupby(['Segment'])[['Sales', 'Profit']].sum().reset_index()
+sales_profit_segment['Sales_to_Profit_Ratio'] = sales_profit_segment['Sales'] / sales_profit_segment['Profit']
+print(sales_profit_segment[['Segment', 'Sales_to_Profit_Ratio']])
